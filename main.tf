c119a187c6a29e7d1e5a643ab3ee76d686d3827c
dallin <dallinwright01@gmail.com>
2022-09-24 11:43:43 +0200

    debugging

--- main.tf
+++ main.tf
@@ -25,3 +25,18 @@ resource "github_repository" "github_repositories" {
     "terraform"
   ]
 }
+
+resource "github_repository" "aws_root" {
+  name        = "aws_root"
+  description = "AWS Root Account Configuration"
+
+  visibility             = "public"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = [
+    "AWS",
+    "Terraform"
+  ]
+}
