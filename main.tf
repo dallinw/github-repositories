9f2a22961c34b5f1bd01cbc656e915742418a0f3
Dallin Wright <dallin.wright@supportlogic.io>
2022-09-30 15:52:48 +0200

    adding rust microservice

--- main.tf
+++ main.tf
@@ -70,3 +70,16 @@ resource "github_repository" "gcp_root" {
     "terraform"
   ]
 }
+
+resource "github_repository" "reservations" {
+  name        = "reservations"
+  description = "Demo reservation project"
+
+  visibility             = "public"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = []
+}
+
