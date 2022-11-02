7b1693e0aebf5df581cb2575f101ea00cbcac520
Dallin Wright <dallin.wright@supportlogic.io>
2022-11-02 14:22:45 +0100

    #comment testing ci

--- main.tf
+++ main.tf
@@ -94,3 +94,15 @@ resource "github_repository" "linkerd_helm_chart" {
 
   topics = []
 }
+
+resource "github_repository" "kubernetes-quickstart" {
+  name        = "kubernetes-quickstart"
+  description = "Quickstart to an enterprise level Kubernetes deployment"
+
+  visibility             = "public"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = []
+}
