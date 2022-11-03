5e4ca1291ebf02531bb2f85a3686114a99803e54
Dallin Wright <dallin.wright@supportlogic.io>
2022-11-03 10:38:58 +0100

    #comment testing ci

--- helm_charts.tf
+++ helm_charts.tf
@@ -21,3 +21,33 @@ resource "github_repository" "ambassador_helm_chart" {
 
   topics = []
 }
+
+resource "github_repository" "platform_deployment" {
+  name        = "platform-deployment"
+  description = "Helm chart for holistic platform deployment"
+
+  visibility             = "public"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = [
+    "helm",
+    "kubernetes"
+  ]
+}
+
+resource "github_repository" "service_deployment" {
+  name        = "service-deployment"
+  description = "Kubernetes stateless service deployment charts"
+
+  visibility             = "public"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = [
+    "helm",
+    "kubernetes"
+  ]
+}
