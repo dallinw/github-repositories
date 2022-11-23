dba861e768422e8fb4a38182b6e3f57c78790623
Dallin Wright <dallin.wright@supportlogic.io>
2022-11-23 14:57:45 +0100

    init

--- main.tf
+++ main.tf
@@ -26,3 +26,17 @@ resource "github_repository" "helm_charts" {
     "kubernetes"
   ]
 }
+
+resource "github_repository" "earnings_strategy_trader" {
+  name        = "earnings-strategy-trader"
+  description = "Earnings Strategy Trader"
+
+  visibility             = "private"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = [
+    "trading"
+  ]
+}
