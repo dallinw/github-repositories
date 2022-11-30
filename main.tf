80409a9956f1b261d550f46a55830881f70caf06
Dallin Wright <dallin.wright@supportlogic.io>
2022-11-30 08:30:11 -0700

    init

--- main.tf
+++ main.tf
@@ -26,31 +26,3 @@ resource "github_repository" "helm_charts" {
     "kubernetes"
   ]
 }
-
-resource "github_repository" "earnings_strategy_trader" {
-  name        = "earnings-strategy-trader"
-  description = "Earnings Strategy Trader"
-
-  visibility             = "private"
-  auto_init              = true
-  license_template       = "gpl-3.0"
-  delete_branch_on_merge = true
-
-  topics = [
-    "trading"
-  ]
-}
-
-resource "github_repository" "data_scraper" {
-  name        = "data-scraper"
-  description = "Utility to pull and parse data from around the web"
-
-  visibility             = "private"
-  auto_init              = true
-  license_template       = "gpl-3.0"
-  delete_branch_on_merge = true
-
-  topics = [
-    "trading"
-  ]
-}
--- services.tf
+++ services.tf
@@ -9,3 +9,32 @@ resource "github_repository" "reservations" {
 
   topics = []
 }
+
+resource "github_repository" "earnings_strategy_trader" {
+  name        = "earnings-strategy-trader"
+  description = "Earnings Strategy Trader"
+
+  visibility             = "private"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = [
+    "trading"
+  ]
+}
+
+resource "github_repository" "metadata_aggregator" {
+  name        = "metadata_aggregator"
+  description = "Utility to pull and parse data from around the web"
+
+  visibility             = "private"
+  auto_init              = true
+  license_template       = "gpl-3.0"
+  delete_branch_on_merge = true
+
+  topics = [
+    "trading",
+    "batch"
+  ]
+}
